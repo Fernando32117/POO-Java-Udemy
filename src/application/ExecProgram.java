0521ac2d33ba401f24cb6afcc4e7c0d2382c7c23
Francisco Fernando <nando32117@gmail.com>
2023-12-27 14:38:07 -0300

    Commit atividades POO Udemy

---
+++ src/application/ExecProgram.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Calculator;
+
+public class ExecProgram {
+	
+	/*Fazer um programa para ler um valor numérico qualquer, e daí mostrar
+	quanto seria o valor de uma circunferência e do volume de uma esfera
+	para um raio daquele valor. Informar também o valor de PI com duas
+	casas decimais.*/
+	
+	public static final double PI = 3.14159;
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter radius: ");
+		double radius = sc.nextDouble();
+		
+		double c = Calculator.circumference(radius);
+		
+		double v = Calculator.volume(radius);
+		
+		System.out.printf("Circumference: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n", v);
+		System.out.printf("PI value: %.2f%n", Calculator.PI);
+		
+		
+		sc.close();
+
+	}
+	
+	public static double circumference(double radius) {
+		return 2.0 * PI * radius;
+		
+	}
+	
+	public static double volume(double radius) {
+		return 4.0 * PI * radius * radius * radius / 3.0;
+		
+	}
+
+}
